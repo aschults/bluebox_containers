4005efce5921eb9c3271eb579964afa2df74ae96
schultschik <aschults@gmx.net>
2016-12-31 13:55:19 +0100

    improved ICAP integration?

--- c_icap_server/test.sh
+++ c_icap_server/test.sh
@@ -1,6 +1,6 @@
 docker build -t myicap . || exit
 docker stop icap1
 docker rm icap1
-#docker run  --name=icap1 -ti -e CLAMD_HOST=bigbox -p 11344:1344 --entrypoint=sh myicap
-docker run  --name=icap1 -ti -e CLAMD_HOST=bigbox -p 11344:1344 myicap
+docker run  --name=icap1 -ti -e CLAMD_HOST=bigbox -p 11344:1344 --entrypoint=sh myicap
+#docker run  --name=icap1 -ti -e CLAMD_HOST=bigbox -p 11344:1344 myicap
 
