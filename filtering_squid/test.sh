6e13a6c728e4c92ffc3fa5c63e41f24c50b1bf9c
schultschik <aschults@gmx.net>
2016-12-31 16:46:58 +0100

    c-icap by IP only

--- filtering_squid/test.sh
+++ filtering_squid/test.sh
@@ -1,7 +1,7 @@
-docker build -t mysquid . 
+docker build --no-cache -t mysquid . 
 docker stop testsq1 
 docker rm testsq1 
 #ep="-ti --entrypoint=sh"
-docker run -e disk_cache_size=200 -e sslbump=1 --name=testsq1 -v /var/tmp/squiddisk2:/var/spool/squid/cache -v /var/tmp/squidssl:/var/spool/squid/ssl -v $PWD/tst_conf:/var/lib/squidGuard/conf.d -p 3333:3128 mysquid 
+docker run -e icap_req_a=icap://10.0.10.20:1344/squidclamav -e disk_cache_size=200 -e sslbump=1 --name=testsq1 -v /var/tmp/squiddisk2:/var/spool/squid/cache -v /var/tmp/squidssl:/var/spool/squid/ssl -v $PWD/tst_conf:/var/lib/squidGuard/conf.d -p 3333:3128 mysquid 
 
 
